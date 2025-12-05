1b9ebfdd7be1d7d1c090672b3453f6942828cffb
masenjo <masenjo@portatilAcer.com>
2025-12-05 13:03:34 +0100

    lined signals to their slots

--- Python/GaleriaImagenes/app/galeria_app.py
+++ Python/GaleriaImagenes/app/galeria_app.py
@@ -4,14 +4,15 @@ from pathlib import Path
 
 from PySide6.QtCore import QFile, QIODevice
 from PySide6.QtGui import QAction
+from PySide6.QtPrintSupport import QPrintDialog
 from PySide6.QtUiTools import QUiLoader
-from PySide6.QtWidgets import QMainWindow, QLabel, QListWidget, QWidget
+from PySide6.QtWidgets import QMainWindow, QLabel, QListWidget, QWidget, QDialog, QDialogButtonBox
 
 from datos.DatosGaleria import UI_DIR
 
 class GaleriaApp:
     """
-    App mínima que carga los .ui en runtime (QUiLoader) y conecta señales a stubs.
+    App mínima que carga los archivos .ui en runtime (QUiLoader) y conecta señales a stubs.
     Mantiene referencias a widgets encontrados vía objectName.
     """
     def __init__(self) -> None:
@@ -20,12 +21,6 @@ class GaleriaApp:
         # Cargamos la ventana principal
         self.window: QMainWindow = self._load_ui(UI_DIR / "ventana_principal.ui")
         self.window.setWindowTitle("Galería Imagenes Mario Asenjo 2 DAM")
-
-        # Configura el carousel, lógica que en el ui no está
-        self._configurar_carousel()
-
-        # Conectamos las señales de momento a stubs
-        self._conectar_signals()
 
         # Lista de imágenes e índice actual de imagen
         self.imagenes: list[Path] = []
@@ -42,9 +37,28 @@ class GaleriaApp:
             # accion menú acerca de...
         self.accion_acerca_de: QAction | None = self.window.findChild(QAction, "actionAcerca_de")
 
-        self._load_carousel()
+        # Validamos la importación de la UI
+        self._verificar_importaciones()
 
-    def _load_ui(self, path: Path) -> QWidget | QMainWindow:
+        # Configura el carousel, lógica que en el ui no está
+        self._configurar_carousel()
+
+        # Conectamos las señales de momento a stubs
+        self._conectar_signals()
+
+    def _verificar_importaciones(self) -> None:
+        for nombre, objeto in {
+            "label_main": self.label_main,
+            "list_thumbnails": self.list_thumbnails,
+            "accion_abrir_carpeta": self.accion_abrir_carpeta,
+            "accion_imprimir": self.accion_imprimir,
+            "accion_salir": self.accion_salir,
+            "accion_acerca_de": self.accion_acerca_de
+        }.items():
+            if objeto is None:
+                raise RuntimeError(f"No se ha encontrado '{nombre}' en ventana_principal.ui")
+
+    def _load_ui(self, path: Path) -> QDialog | QMainWindow:
         archivo: QFile = QFile(str(path))
         if not archivo.open(QIODevice.OpenModeFlag.ReadOnly):
             raise RuntimeError(f"No se pudo abrir el .ui -> {path}")
@@ -54,8 +68,52 @@ class GaleriaApp:
             raise RuntimeError(f"QUiLoader.load() ha devuelto None!!")
         return widget
 
-    def _configurar_carousel(self):
-        pass
+    def _configurar_carousel(self) -> None:
+        # Como en el Designer no he confirado nada más el tamaño de cada icono y cada celda,
+        # aquí se configura el funcionamiento del carousel como tal
+        self.list_thumbnails.setViewMode(QListWidget.ViewMode.IconMode)
+        self.list_thumbnails.setFlow(QListWidget.Flow.LeftToRight)
+        self.list_thumbnails.setMovement(QListWidget.Movement.Static)
+        self.list_thumbnails.setResizeMode(QListWidget.ResizeMode.Adjust)
+        self.list_thumbnails.setSelectionMode(QListWidget.SelectionMode.SingleSelection)
 
-    def _conectar_signals(self):
-        pass
+    def on_abrir_carpeta(self) -> None:
+        self.window.statusBar().showMessage("Abrir carpeta ...", 2000)
+
+    def on_imprimir_imagen(self) -> None:
+        # Realmente no vamos a imprimir dice Luis, así que solo mostramos el dialogo de impresion.
+        dialogo_impresion: QPrintDialog = QPrintDialog(self.window)
+        dialogo_impresion.exec()
+
+    def on_acerca_de(self) -> None:
+        dialogo_acerca: QDialog = self._load_ui(UI_DIR / "about_dialog.ui")
+        button_box: QDialogButtonBox = dialogo_acerca.findChild(QDialogButtonBox, "buttonBox")
+        if button_box:
+            button_box.rejected.connect(dialogo_acerca.reject)
+
+        # Rellenamos los labels
+        nombre: QLabel = dialogo_acerca.findChild(QLabel, "nombre_app_label")
+        autor: QLabel = dialogo_acerca.findChild(QLabel, "autor_label")
+        info: QLabel = dialogo_acerca.findChild(QLabel, "info_app_label")
+        if nombre: nombre.setText("Galería de Imágenes - v1.0")
+        if autor: autor.setText("Autor: Mario Asenjo Pérez - 2º DAM - UAX")
+        if info: info.setText("Proyecto de práctica con PySide6 y Designer de Widgets.\nRepositorio en: https://github.com/mario-asenjo/DAM2-DI/tree/main/Python/GaleriaImagenes .")
+
+        # Hacemos que el dialogo sea modal
+        dialogo_acerca.setModal(True)
+        dialogo_acerca.exec()
+
+    def on_cambio_de_tumbnail(self, row: int):
+        if row < 0:
+            return
+        self.window.statusBar().showMessage(f"Seleccionaste miniatura ${row}", 1500)
+
+    def _conectar_signals(self) -> None:
+        # Menú
+        self.accion_abrir_carpeta.triggered.connect(self.on_abrir_carpeta)
+        self.accion_imprimir.triggered.connect(self.on_imprimir_imagen)
+        self.accion_salir.triggered.connect(self.window.close)
+        self.accion_acerca_de.triggered.connect(self.on_acerca_de)
+
+        # Carousel
+        self.list_thumbnails.currentRowChanged.connect(self.on_cambio_de_tumbnail)
